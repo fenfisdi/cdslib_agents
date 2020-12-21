1b86695e2653b67b73469ac2bfc9f11f46efe878
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-12-21 13:57:47 -0500

    implemented behaviour.move_agent

--- agents/behaviour.py
+++ agents/behaviour.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 def move_agent(df: pd.DataFrame, dt: float) -> pd.DataFrame:
-    raise NotImplementedError
+    aux_df = df.copy()
+    aux_df['x'] = aux_df.apply(lambda row: row['x'] + row['vx'] * dt, axis=1)
+    aux_df['y'] = aux_df.apply(lambda row: row['y'] + row['vy'] * dt, axis=1)
+    return aux_df
