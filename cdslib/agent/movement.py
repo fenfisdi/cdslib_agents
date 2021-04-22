2a1c67e04996b7f77c82f35b272595329bd48a63
David Peña <dpenah@intergrupo.com>
2021-04-22 09:19:19 -0500

    Chore: Create Agent Movement

---
+++ cdslib/agent/movement.py
@@ -0,0 +1,28 @@
+from cdslib.models.population import BoxSize
+
+
+class AgentMovement:
+
+    @classmethod
+    def apply_movement(cls, dataframe, box_size: BoxSize):
+        """
+
+        """
+        dataframe.x += dataframe.vx
+        dataframe.y += dataframe.vy
+
+        if dataframe.x < box_size.left:
+            dataframe.vx = -dataframe.vx
+            dataframe.x = box_size.left
+        if dataframe.x > box_size.right:
+            dataframe.vx = -dataframe.vx
+            dataframe.x = box_size.right
+
+        if dataframe.y < box_size.bottom:
+            dataframe.vy = -dataframe.vy
+            dataframe.y = box_size.bottom
+        if dataframe.y > box_size.top:
+            dataframe.vy = -dataframe.vy
+            dataframe.y = box_size.top
+
+        return dataframe
