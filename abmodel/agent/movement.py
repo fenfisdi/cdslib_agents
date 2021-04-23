5b16c587373841cd5b9da353f9e9bd1d1e9f09ba
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-04-23 10:28:48 -0500

    Change root directory name. Including columns validation for movement models

---
+++ abmodel/agent/movement.py
@@ -0,0 +1,83 @@
+from pandas.core.frame import DataFrame
+
+from cdslib.models.population import BoxSize
+
+
+class AgentMovement:
+
+    @classmethod
+    def apply_movement(
+        cls, df: DataFrame, box_size: BoxSize, dt: float
+    ):
+        """
+        Function to apply as transformation in a pandas Dataframe to update
+        coordinates from the agent with its velocities.
+
+        Parameters
+        ----------
+        df: DataFrame
+            Dataframe to apply transformation, must have x, y, vx, vy columns.
+
+        box_size: BoxSize
+            Parameter according to the region coordinates.
+
+        dt: float
+            Local time step, representing how often to take a measure.
+
+        Returns
+        -------
+        DataFrame
+            Dataframe with the transformations in columns x and y
+        """
+        try:
+            # Update current position of the agent with its velocities
+            df.x += df.vx * dt
+            df.y += df.vy * dt
+
+            # Verify if coordinates are out of the box
+            # then return to the box limit
+            if df.x < box_size.left:
+                df.vx = -df.vx
+                df.x = box_size.left
+            if df.x > box_size.right:
+                df.vx = -df.vx
+                df.x = box_size.right
+
+            if df.y < box_size.bottom:
+                df.vy = -df.vy
+                df.y = box_size.bottom
+            if df.y > box_size.top:
+                df.vy = -df.vy
+                df.y = box_size.top
+
+            return df
+        except Exception:
+            df_cols = df.columns
+            x_check = 'x' in df_cols
+            y_check = 'y' in df_cols
+            vx_check = 'vx' in df_cols
+            vy_check = 'vy' in df_cols
+
+            if (not x_check or not y_check or not vx_check or not vy_check):
+                check_string = ""
+
+                if not x_check:
+                    check_string += "'x', "
+
+                if not y_check:
+                    check_string += "'y', "
+
+                if not vx_check:
+                    check_string += "'vx', "
+
+                if not vy_check:
+                    check_string += "'vy', "
+
+                check_string += "must be checked."
+
+                error_string = (
+                    "df must contain 'x', 'y', 'vx' "
+                    "and 'vy' columns. "
+                    )
+
+                raise ValueError(error_string + check_string)
--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-"""
-Script usado para la configuracion e instalacion del paquete
-"""
-
 from setuptools import find_packages, setup
 
 
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
 
-setup(name='cdslib',
+setup(name='cdslib_agents',
       version='0.1.0',
       maintainer='Developers of the CDS team of FEnFiSDi group',
       maintainer_email='camilo.hincapie@udea.edu.co',
       description='Library for contagious diseases simulation',
       long_description=long_description,
-      url='https://github.com/fenfisdi/cdslib',
+      url='https://github.com/fenfisdi/cdslib_agents',
       author='Camilo Hincapié Gutiérrez',
       author_email='camilo.hincapie@udea.edu.co',
       license='GNU General Public License v3 (GPLv3)',
@@ -28,9 +24,6 @@ setup(name='cdslib',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
@@ -38,11 +31,16 @@ setup(name='cdslib',
         'Topic :: Scientific/Engineering :: Medical Science Apps.',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries'
-      ],
+        ],
       test_suite='pytest',
       packages=find_packages(exclude='__pycache__'),
-      keywords=['Complex Systems', 'Computation', 'Epidemiology',
-                'Nonlinear dynamics'],
-      python_requires='>=3.5',
+      keywords=[
+        'Complex Systems',
+        'Computation',
+        'Epidemiology',
+        'Nonlinear dynamics',
+        'Agent Based Models'
+        ],
+      python_requires='>=3.8',
       install_requires=['numpy', 'pandas>=1.1.1', 'plotly>=4.10.0', 'scipy'],
       zip_safe=False)
