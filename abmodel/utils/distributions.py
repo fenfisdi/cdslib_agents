02f421cb0285a38de38d078e395d469b56fe1660
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-04-24 17:49:41 -0500

    WIP: adding sample_one method in distributions

--- abmodel/utils/distributions.py
+++ abmodel/utils/distributions.py
@@ -1,5 +1,4 @@
-from numpy import random
-import pandas as pd
+from numpy import random, genfromtxt
 
 from sklearn.neighbors import KernelDensity
 
@@ -7,11 +6,35 @@ from sklearn.neighbors import KernelDensity
 class Distribution:
     """
     """
-    def __init__(self, type: str, **kwargs, n):
-        if type is "empirical":
-            print("test")
+    def __init__(self, dist_type: str, filename: str = "", **kwargs):
+        """
+        """
+        self.dist_type = dist_type
+        self.kwargs = kwargs
+
+        if dist_type == "empirical":
+            try:
+                data = genfromtxt(filename)
+                self.kd_estimator = KernelDensity(**kwargs).fit(data)
+            except Exception as error:
+                raise ValueError(
+                    f"Error parsing file: {filename}\n"
+                    + error
+                    )
         else:
-            if type in dir(random):
-                distribution = getattr(random, type)
+            if dist_type in dir(random):
+                self.distribution = getattr(random, dist_type)
             else:
-                raise ValueError()
+                raise ValueError(
+                    "'dist_type' is not implemented in "
+                    "numpy.random"
+                    )
+
+    def sample_one(self):
+        """
+        """
+        if self.dist_type == "empirical":
+            return self.kd_estimator()
+        else:
+            # Using numpy.random
+            return self.distribution(**self.kwargs)[0]
