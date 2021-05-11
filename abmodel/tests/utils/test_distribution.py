86d5e3023baed33364782dc8f3292ef01860d800
IanMejia <iankilljoy23@gmail.com>
2021-05-11 15:27:54 -0500

    Chore: distributions tests

---
+++ abmodel/tests/utils/test_distribution.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+import numpy as np
+
+from abmodel.utils.distributions import Distribution
+
+class DistributionTest(TestCase):
+
+    def setUp(self) -> None:
+        //TODO
