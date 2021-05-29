b551aebadc4d74496908749ee4c8e5b5ab738a8b
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-05-29 16:11:39 -0500

    Improving test readability

--- abmodel/agent/movement.py
+++ abmodel/agent/movement.py
@@ -13,7 +13,7 @@ class AgentMovement:
     @classmethod
     def move_agents(
         cls, df: DataFrame, box_size: BoxSize, dt: float
-    ) -> None:
+    ) -> DataFrame:
         """
             Function to apply as transformation in a pandas Dataframe to update
             coordinates from the agent with its velocities.
@@ -46,44 +46,42 @@ class AgentMovement:
 
         """
 
-        def move_individual_agent(df):
-
-                #Analizando función de movimiento
-                
-                # Update current position of the agent with its velocities
-            df.x += df.vx * dt
-            df.y += df.vy * dt
+        def move_individual_agent(row):
+            """
+            A row represents an agent
+            """
+            # Update current position of the agent with its velocities
+            row.x += row.vx * dt
+            row.y += row.vy * dt
 
             # Verify if coordinates are out of the box
             # then return to the box limit
-            if df.x < box_size.left:
-                df.vx = -df.vx
-                df.x = box_size.left
-            if df.x > box_size.right:
-                df.vx = -df.vx
-                df.x = box_size.right
+            if row.x < box_size.left:
+                row.vx = -row.vx
+                row.x = box_size.left
+            if row.x > box_size.right:
+                row.vx = -row.vx
+                row.x = box_size.right
 
-            if df.y < box_size.bottom:
-                df.vy = -df.vy
-                df.y = box_size.bottom
-            if df.y > box_size.top:
-                df.vy = -df.vy
-                df.y = box_size.top
-            
-            return df
+            if row.y < box_size.bottom:
+                row.vy = -row.vy
+                row.y = box_size.bottom
+            if row.y > box_size.top:
+                row.vy = -row.vy
+                row.y = box_size.top
+
+            return row
 
         check_field_errors(df)
         try:
-            #print('fefsa')
-            df = df.apply(move_individual_agent, axis=1,)
-            #print('fgbfj')
+            df = df.apply(move_individual_agent, axis=1)
         except Exception:
             check_field_existance(df, ["x", "y", "vx", "vy"])
-            
+
         return df
 
     @classmethod
-    def stop_agents(cls, df: DataFrame, indexes: list) -> None:
+    def stop_agents(cls, df: DataFrame, indexes: list) -> DataFrame:
         """
             Set the velocity of a given set of agents to zero.
 
@@ -100,26 +98,25 @@ class AgentMovement:
         if check_field_existance(df, ["vx", "vy"]):
             df.loc[indexes, "vx"] = 0
             df.loc[indexes, "vy"] = 0
-            
+
         return df
 
     @classmethod
-    def vector_angles(cls, df: DataFrame, components: list) -> Any:
+    def vector_angles(cls, df: DataFrame, components: list) -> DataFrame:
         """
-            Set the direction of a given set of agents to zero.
+            Set ... TODO
 
             Parameters
             ----------
             df : DataFrame
                 Dataframe to apply transformation
-        
+
             Components: list ??????????????????????? vel or pos
             ----------
         """
         def angle(x: float, y: float) -> float:
             try:
-                #return arctan(y/x)
-                return arctan2(y, x)          #####mejor usar arctan2 para que no devuelva ángulo envuelto
+                return arctan2(y, x)
             except Exception:
                 return 0.0
 
@@ -128,13 +125,12 @@ class AgentMovement:
                 lambda row: angle(row[components[0]], row[components[1]]),
                 axis=1
                 )
-
         return df
-    
+
     @classmethod
     def update_velocities(cls, df: DataFrame, distribution: Distribution,
                           angle_variance: float, group_field: str = "",
-                          group_label: str = "") -> None:
+                          group_label: str = "") -> DataFrame:
         """
             Set the velocity of a given set of agents to zero.
 
@@ -143,13 +139,11 @@ class AgentMovement:
             df : DataFrame
                 Dataframe to apply transformation, must have ...
         """
-        
-        
-        def change_velocities(df):
+        def change_velocities(df, angle_variance):
             """
             """
             n_agents = len(df.index)
-            new_velocities = distribution.sample(size=n_agents)
+            new_velocities_norm = distribution.sample(size=n_agents)
 
             angles = cls.vector_angles(df, ["vx", "vy"])
 
@@ -162,29 +156,27 @@ class AgentMovement:
 
             angles = angles + delta_angles
 
-            df.loc["vx"] = new_velocities * cos(angles)
+            df.loc["vx"] = new_velocities_norm * cos(angles)
 
-            df.loc["vy"] = new_velocities * sin(angles)
-            print('cambio vel')
+            df.loc["vy"] = new_velocities_norm * sin(angles)
+
             return df
 
-        print('entra a if')
         if check_field_existance(df, ["vx", "vy"]) and group_field == "":
-            print('entrando a change vel')
-            df = change_velocities(df)
+            df = change_velocities(df, angle_variance)
 
         if group_field != "":
             if check_field_existance(df, [group_field, "vx", "vy"]):
                 if group_label in df[group_field].values:
                     filtered_df = df.loc[df[group_field] == group_label]
-                    change_velocities(filtered_df)
+                    # Update df using filtered_df
+                    df.update(change_velocities(filtered_df, angle_variance))
                 else:
                     pass
         return df
 
-       
     @classmethod
-    def avoid_agents(cls, df: DataFrame, df_to_avoid: DataFrame) -> None:
+    def avoid_agents(cls, df: DataFrame, df_to_avoid: DataFrame) -> DataFrame:
         """
         """
         def deviation_angle(grouped_serie: Series) -> float:
@@ -209,10 +201,10 @@ class AgentMovement:
                     greatest_angle_to_avoid["consecutive_angle"].iloc[0]/2) % 2*pi
 
         def replace_velocities(row, new_angles):
-            if row["agent"] in new_angles.index:
-                velocity_norm = sqrt(row["vx"]**2 + row["vy"]**2)
-                row["vx"] = velocity_norm * cos(new_angles[row["agent"]])
-                row["vy"] = velocity_norm * sin(new_angles[row["agent"]])
+            if row.agent in new_angles.index:
+                velocity_norm = sqrt(row.vx**2 + row.vy**2)
+                row.vx = velocity_norm * cos(new_angles[row.agent])
+                row.vy = velocity_norm * sin(new_angles[row.agent])
             return row
 
         if check_field_existance(df, ["agent", "x", "y", "vx", "vy"]):
@@ -235,11 +227,11 @@ class AgentMovement:
                             on="agent_to_avoid"
                             )
             scary_agents["x_relative"] = scary_agents.apply(
-                    lambda row: row["x_to_avoid"] - row["x"], axis=1
+                    lambda row: row.x_to_avoid - row.x, axis=1
                     )
 
             scary_agents["y_relative"] = scary_agents.apply(
-                    lambda row: row["y_to_avoid"] - row["y"], axis=1
+                    lambda row: row.y_to_avoid - row.y, axis=1
                     )
 
             scary_agents["relative_angle"] = cls.vector_angles(
@@ -255,7 +247,7 @@ class AgentMovement:
             new_angles = scary_agents[["agent", "relative_angle"]] \
                 .groupby("agent").apply(deviation_angle)
 
-            df = df.apply(
+            return df.apply(
                 lambda row: replace_velocities(row, new_angles),
                 axis=1
                 )
--- abmodel/tests/agent/test_movement.py
+++ abmodel/tests/agent/test_movement.py
@@ -9,12 +9,20 @@ from abmodel.utils.distributions import Distribution
 
 
 class AgentMovementTestCase(TestCase):
-
+    """
+    """
     def setUp(self):
+        """
+        """
         self.box_size = BoxSize(-50, 50, -30, 30)
         self.dt = 1.0
 
-    def test_movement_function(self):
+    def test_movement_function_1(self):
+        """
+        """
+        print("\n")
+        print("Test #1: Change all agents' positions")
+    
         samples = 50
         iterations = 10
 
@@ -30,84 +38,93 @@ class AgentMovementTestCase(TestCase):
             df = AgentMovement.move_agents(df, self.box_size, self.dt)
 
         self.assertNotEqual(data.get('x')[0], df['x'][0])
-        
-        
 
-    def test_movement_walls(self):
-        #samples = 1
-        iterations = 1
+    def test_movement_function_2(self):
+        """
+        """
+        print("\n")
+        print("Test #2: Change just one agent position")
 
         data = {
             'x': np.array([0]),
             'y': np.array([0]),
-            'vx': np.array([1.0]),  #cambia resultado si se pone entero o float (vel = 1 o a 1.0 para dt = 0.5
+            'vx': np.array([1.0]),
             'vy': np.array([0]),
-        }
+            }
         df = pd.DataFrame(data)
 
-        for _ in range(iterations):
-             df = AgentMovement.move_agents(df, self.box_size, self.dt)
-            #print('iteré:')
-        print(df)
-        self.assertAlmostEqual(1.0, df['x'][0])        
-        
-        
+        df = AgentMovement.move_agents(df, self.box_size, self.dt)
+
+        self.assertAlmostEqual(1.0, df['x'][0])
 
     def test_stop_agents(self):
+        """
+        """
+        print("\n")
+        print("Test: Change just one agent position")
+
         samples = 2
-        iterations = 10
 
         data = {
-            'x': np.random.randint(-20, 20, samples),
-            'y': np.random.randint(-20, 20, samples),
             'vx': -20 * np.random.random(samples) + 10,
             'vy': -20 * np.random.random(samples) + 10,
         }
         df = pd.DataFrame(data)
 
-        for _ in range(iterations):
-            df = AgentMovement.stop_agents(df, [1])
-            
+        print("Before applying stop_agents:")
+        print(df)
+
+        df = AgentMovement.stop_agents(df, [1])
+
+        print("After applying stop_agents stopping 2nd agent (index=1):")
+        print(df)
+
         cond1 = df['vx'][1] == 0.0
         cond2 = df['vy'][1] == 0.0
 
-        self.assertTrue(cond1 and cond2) 
-    
-    
+        self.assertTrue(cond1 and cond2)
+
     def test_vector_angles(self):
+        """
+        """
+        print("\n")
+        print("Test: Calculate vector direction adequately")
 
         data = {
-            'x': np.array([0]),
-            'y': np.array([0]),
-            'vx': np.array([-1.0]),  
-            'vy': np.array([1.0]),
-        }
+            'x': np.array([-1.0]),
+            'y': np.array([1.0]),
+            }
         df = pd.DataFrame(data)
 
-        df = AgentMovement.vector_angles(df, ['vx', 'vy'])
-            #print('iteré:')
+        print("The vector points towards 3*pi/4:")
         print(df)
-        self.assertAlmostEqual(3*np.pi/4, df[0]) 
-        
-    
+
+        angles = AgentMovement.vector_angles(df, ['x', 'y'])
+
+        print("The calculated angle is:")
+        print(angles)
+        self.assertAlmostEqual(3*np.pi/4, angles[0])
+
     def test_update_velocities(self):
+        """
+        """
+        print("\n")
+        print("Test: Calculate vector direction adequately")
 
         data = {
             'x': np.array([0]),
             'y': np.array([0]),
-            'vx': np.array([1.0]),  
+            'vx': np.array([1.0]),
             'vy': np.array([0]),
-        }
+            }
         df = pd.DataFrame(data)
-        Distrib = Distribution(
-                dist_type="numpy",
-                distribution="normal",
-                loc=0.0,
-                scale=1.0   #angle_variance
-                )
-            #print('iteré:')
-        df = AgentMovement.update_velocities(df, Distrib, 1.0)
+        distrib = Distribution(
+            dist_type="numpy",
+            distribution="normal",
+            loc=0.0,
+            scale=1.0
+            )
+        #print('iteré:')
+        df = AgentMovement.update_velocities(df, distrib, 1.0)
         print(df)
         self.assertNotEqual(1.0, df['vx'][0])        
-               
-        
