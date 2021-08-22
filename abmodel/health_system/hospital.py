32e877fca131eea7c278eab8a1040218c293310f
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-08-22 15:52:05 -0500

    WIP: working in health system implementation

---
+++ abmodel/health_system/hospital.py
@@ -0,0 +1,10 @@
+class Hospital:
+    """
+        ... TODO
+    """
+    def __init__(self, hospital_capacity: int, ICU_capacity: int) -> None:
+        """
+            TODO
+        """
+        self.hospital_capacity = hospital_capacity
+        self.ICU_capacity = ICU_capacity
--- abmodel/models/health_system.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class HealthSystem(BaseModel):
-    """
-        TODO
-    """
-    hospital_capacity: int
-    ICU_capacity: int
