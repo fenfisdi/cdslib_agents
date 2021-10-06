43ef41de05a797a7e6f812a86c8f805c4a31a398
VLADIMIR EMIL RUEDA GÓMEZ <vladimir.rueda@udea.edu.co>
2021-10-05 23:37:26 -0500

    Correction of the tests files using the tool for style guide flake8

--- abmodel/tests/agent/test_movement.py
+++ abmodel/tests/agent/test_movement.py
@@ -1,30 +1,27 @@
 import pytest
-
-from numpy import random, array, nan, all, pi, round, sqrt
 from pandas import DataFrame
+from scipy.stats import kstest
+from numpy import random, array, nan, all, pi, round, sqrt
 
 from abmodel.agent.movement import AgentMovement
 from abmodel.models.population import BoxSize
 from abmodel.utils.distributions import Distribution
-from scipy.stats import kstest
+
 
 class TestCaseAgentMovement:
     """
-        Verify the functionality of all methods in the AgentMovement class
-        from agent, using unitary test with the Python testing tool pytest.
+        Verifies the functionality of all methods in the AgentMovement class
+        from agent.
     """
 
     def setup_method(self, method):
-        print('==>')
-        print(method.__doc__)
-
+        """Allows to see a brief description of the test in the report."""
+        print('\u21B4' + '\n' + '\u273C' + method.__doc__.strip())
 
     @pytest.fixture
-    def setUp(self, autouse = True) -> None:
-
+    def set_up(self, autouse=True) -> None:
         pytest.box_size = BoxSize(-50, 50, -30, 30)
         pytest.dt = 1.0
-
         pytest.data = {
             'x': [0],
             'y': [0],
@@ -40,8 +37,7 @@ class TestCaseAgentMovement:
             }
 
     @pytest.fixture
-    def fixture_crash_with_wall(self, setUp, scope = 'method') -> None:
-
+    def fixture_crash_with_wall(self, set_up, scope='method') -> None:
         pytest.data = {
             'x': [49, 0, -49, 0],
             'y': [0, 29, 0, -29],
@@ -57,8 +53,7 @@ class TestCaseAgentMovement:
             }
 
     @pytest.fixture
-    def fixture_crash_with_corner(self, setUp, scope = 'method') -> None:
-
+    def fixture_crash_with_corner(self, set_up, scope='method') -> None:
         pytest.data = {
             'x': [49, -49, -49, 49],
             'y': [29, 29, -29, -29],
@@ -74,8 +69,7 @@ class TestCaseAgentMovement:
             }
 
     @pytest.fixture
-    def fixture_stop_agents(self, scope = 'method') -> None:
-
+    def fixture_stop_agents(self, scope='method') -> None:
         samples = 10
         sample_stopped = 5
         pytest.data = {
@@ -89,8 +83,7 @@ class TestCaseAgentMovement:
         }
 
     @pytest.fixture
-    def fixture_test_vector_angles_raise_error(self, scope = 'method') -> None:
-
+    def fixture_test_vector_angles_raise_error(self, scope='method') -> None:
         pytest.data = {
             'x': [0],
             'y': [0],
@@ -98,16 +91,15 @@ class TestCaseAgentMovement:
             'vy': [0],
             }
 
-        pytest.wrong_list = ['X' , 'vx']
+        pytest.wrong_list = ['X', 'vx']
 
     @pytest.fixture
-    def fixture_update_velocities(self, scope = 'class') -> None:
-
+    def fixture_update_velocities(self, scope='class') -> None:
         pytest.angle_variance = 0.1
         samples = 500
         pytest.data = {
-            'vx': -20*random.random(samples) +10,
-            'vy': -20*random.random(samples) +10
+            'vx': -20*random.random(samples) + 10,
+            'vy': -20*random.random(samples) + 10
         }
 
         pytest.distrib = Distribution(
@@ -121,8 +113,7 @@ class TestCaseAgentMovement:
             dist_type="numpy",
             dist_name="normal",
             loc=0.0,
-            scale=pytest.angle_variance
-            ).sample(size=samples)
+            scale=pytest.angle_variance).sample(size=samples)
 
         pytest.data_Campo = {
             'Campo': array([0, 1]),
@@ -138,8 +129,7 @@ class TestCaseAgentMovement:
         }
 
     @pytest.fixture
-    def fixture_avoid_agents(self, scope = 'class'):
-
+    def fixture_avoid_agents(self, scope='class'):
         pytest.data_df1 = {
             'agent': [1, 2, 3],
             'x': [0, 1, 1],
@@ -160,28 +150,23 @@ class TestCaseAgentMovement:
             'vy': [0, 2.0, 0],
             }
 
-    def test_movement_function(self,setUp):
+    def test_movement_function(self, set_up):
         """Change just the x agent position on the movement_function."""
-
         df = DataFrame(pytest.data)
         df = AgentMovement.move_agents(df, pytest.box_size, pytest.dt)
 
         assert 1.0 == df['x'][0]
 
-
     def test_crash_with_boundary_wall_(self, fixture_crash_with_wall):
         """Crash of one agent with a box boundary wall."""
-
         df = DataFrame(pytest.data)
         df = AgentMovement.move_agents(df, pytest.box_size, pytest.dt)
         expected_df = DataFrame(pytest.expected_data)
 
         assert all(df == expected_df) == True
-
 
     def test_crash_with_boundary_corner(self, fixture_crash_with_corner):
         """Crash of one agent with a box boundary corner."""
-
         df = DataFrame(pytest.data)
         df = AgentMovement.move_agents(df, pytest.box_size, pytest.dt)
 
@@ -189,100 +174,92 @@ class TestCaseAgentMovement:
 
         assert all(df == expected_df) == True
 
-
-    def test_movement_function_field_error(self, setUp):
-        """Raise an exception when the input DataFrame has na values."""
-
+    def test_movement_function_field_error(self, set_up):
+        """Raises an exception when the input DataFrame has na values."""
         df = DataFrame(pytest.data_na)
 
         with pytest.raises(ValueError):
             assert AgentMovement.move_agents(df, pytest.box_size, pytest.dt)
 
-
     def test_stop_agents(self, fixture_stop_agents):
-        """Stop agent movement with a correct list of index."""
-
+        """Stops agent movement with a correct list of index."""
         df = DataFrame(pytest.data)
         df = AgentMovement.stop_agents(df, pytest.list_index_stopped)
 
         for i in range(len(pytest.list_index_stopped)):
             assert df['vx'][i] == df['vy'][i] == 0
 
-
     def test_stop_agents_raise_Exception(self, fixture_stop_agents):
-        """Raise an exception when the input DataFrame columns 'vx' and 'vy' does not exists."""
-
+        """
+        Raises an exception when the input DataFrame columns 'vx' and 'vy'
+        does not exist.
+        """
         df = DataFrame(pytest.data_wrong_name_columns)
 
         with pytest.raises(ValueError):
             assert AgentMovement.stop_agents(df, pytest.list_index_stopped)
 
-
     @pytest.mark.parametrize(
-        "input_angle, expected_angle",[
-        (13*pi/4, 5*pi/4),
-        (-pi/4, 7*pi/4),
-        (0, 0)
-        ],
-        ids=['13*pi/4 - 5*pi/4', '-pi/4 - 7*pi/4', '0 - 0']
+        "input_angle, expected_angle",
+        [(13*pi/4, 5*pi/4),
+         (-pi/4, 7*pi/4),
+         (0, 0)], ids=['13*pi/4 - 5*pi/4', '-pi/4 - 7*pi/4', '0 - 0']
     )
     def test_standardize_angle(self, input_angle, expected_angle):
-        """Verify standarization on the interval [0, 2*pi]"""
-
-        assert round(AgentMovement.standardize_angle(input_angle), decimals =10) == round(expected_angle, decimals =10)
-
+        """Verifies standardization on the interval [0, 2*pi]."""
+        assert round(AgentMovement.standardize_angle(input_angle),
+                     decimals=10) == round(expected_angle, decimals=10)
 
     @pytest.mark.parametrize(
-        "input_df, expected_angle", [
-        (DataFrame({
-            'x': [-1.0],
-            'y': [1.0],
-            }), 3*pi/4),
-        (DataFrame({
-            'x': [-1.0],
-            'y': [0.0],
-            }), pi)
-    ], ids=['Case 1', 'Case 2']
+        "input_df, expected_angle",
+        [(DataFrame({'x': [-1.0], 'y': [1.0]}), 3*pi/4),
+         (DataFrame({'x': [-1.0], 'y': [0.0]}), pi)], ids=['Case 1', 'Case 2']
     )
     def test_vector_angles(self, input_df, expected_angle):
-        """Calculate vector direction adequately between 'x' and 'y' component of the input DataFrame."""
-
+        """
+        Calculates the vector direction adequately between 'x' and 'y'
+        components of the input DataFrame.
+        """
         angle = AgentMovement.vector_angles(input_df, ['x', 'y'])
 
         assert round(angle[0], decimals =10) == round(expected_angle, decimals =10)
 
-
-    def test_vector_angles_raise_error(self, fixture_test_vector_angles_raise_error):
-        """Raise an exception when the input DataFrame columns 'vx' and 'vy' does not exists."""
-
+    def test_vector_angles_raise_error(self,
+                                       fixture_test_vector_angles_raise_error):
+        """
+        Raises an exception when the input DataFrame columns 'vx' and 'vy'
+        does not exist.
+        """
         df = DataFrame(pytest.data)
         wrong_list = (pytest.wrong_list)
 
         with pytest.raises(ValueError):
             assert AgentMovement.vector_angles(df, wrong_list)
 
-
-    def test_update_velocities_angle_variance_0(self, fixture_update_velocities):
-        """Not variations in the direction of agent (angle_variance = 0.0)."""
-
-
+    def test_update_velocities_angle_variance_0(self,
+                                                fixture_update_velocities):
+        """
+        Verifies wheter there are no changes in the velocities components when
+        the standard deviation of the normal distribution is set to zero
+        (angle_variance=0.0).
+        """
         df = DataFrame(pytest.data)
         angles_before = AgentMovement.vector_angles(df, ['vx', 'vy'])
 
-
         df = AgentMovement.update_velocities(df, pytest.distrib, 0.0)
         angles_after = AgentMovement.vector_angles(df, ['vx', 'vy'])
-
 
         not_angle_variation = \
             all(round(angles_before.head() - angles_after.head(), 7)) == 0
 
         assert not_angle_variation == True
 
-
-    def test_update_velocities_comparation_normal_distribution(self, fixture_update_velocities):
-        """Comparation of the magnitude velocities of agents with a given distribution (normal), through a Kolmogorov-smirnov test."""
-
+    def test_update_velocities_comparation_normal_distribution(
+                                            self, fixture_update_velocities):
+        """
+        Comparison of the velocities magnitude of agents with a given
+        distribution (normal), through a Kolmogorov-smirnov test.
+        """
         df = DataFrame(pytest.data)
         df = AgentMovement.update_velocities(df, pytest.distrib, 0.0)
         velocities_magnitude = sqrt(df['vx']**2 + df['vy']**2)
@@ -294,17 +271,17 @@ class TestCaseAgentMovement:
 
             assert velocity_distrib == True
 
-
     def test_update_velocities_3(self, fixture_update_velocities):
-        """Comparation of the angles of agents with a given distribution (normal), through a Kolmogorov-smirnov test."""
-
+        """
+        Comparison of the angles of the agents with a given distribution
+        (normal), through a Kolmogorov-smirnov test.
+        """
         df = DataFrame(pytest.data)
         angles_before = AgentMovement.vector_angles(df, ['vx', 'vy'])
         df = AgentMovement.update_velocities(
             df, pytest.distrib, pytest.angle_variance
             )
         angles_after = AgentMovement.vector_angles(df, ['vx', 'vy'])
-        #np.savetxt('Data angles_after.csv', angles_after)
         angle_variation = angles_after - angles_before
         k, p = kstest(angle_variation.values, pytest.delta_angles)
         if p > 0.05:
@@ -314,10 +291,8 @@ class TestCaseAgentMovement:
 
         assert angle_variation == True
 
-
     def test_update_velocities_under_field(self, fixture_update_velocities):
-        """Update velocities only for agents under a field."""
-
+        """Updates velocities only for agents under a field."""
         df = DataFrame(pytest.data_Campo)
         df = AgentMovement.update_velocities(
             df, pytest.distrib, pytest.angle_variance_Campo,
@@ -329,19 +304,19 @@ class TestCaseAgentMovement:
 
         assert velocities_field_vx and velocities_field_vy == True
 
-
     def test_update_velocities_raise_error(self, fixture_update_velocities):
-        """Raise an exception when the input DataFrame columns 'vx' and 'vy' does not exists."""
-
+        """
+        Raises an exception when the input DataFrame columns 'vx' and 'vy'
+        does not exist.
+        """
         df = DataFrame(pytest.data_wrong_name_columns)
 
         with pytest.raises(ValueError):
-            assert AgentMovement.update_velocities(df, pytest.distrib, pytest.angle_variance)
-
+            assert AgentMovement.update_velocities(
+                                    df, pytest.distrib, pytest.angle_variance)
 
     def test_avoid_agents(self, fixture_avoid_agents):
-        """Avoid one agent of a DataFrame."""
-
+        """Avoids one agent of a DataFrame."""
         df = DataFrame(pytest.data_df1)
         df_to_avoid = DataFrame(pytest.data_df2)
         new_df = AgentMovement.avoid_agents(df, df_to_avoid)
@@ -350,10 +325,11 @@ class TestCaseAgentMovement:
 
         assert condition == True
 
-
         def test_avoid_agents_raise_error(self, fixture_avoid_agents):
-            """Raise an exception when the input DataFrame column 'agent' does not exists"""
-
+            """
+            Raises an exception when the input DataFrame column 'agent'
+            does not exist.
+            """
             df = DataFrame(pytest.data_whitout_agent)
             df_to_avoid = DataFrame(pytest.data_df2)
 
--- abmodel/tests/utils/test_distribution.py
+++ abmodel/tests/utils/test_distribution.py
@@ -1,21 +1,17 @@
 import pytest
-import re
-import numpy as np
+from numpy import random, ones, abs
 
 from abmodel.utils.distributions import Distribution
 
 
-
 class TestDistribution():
-
     """
-        Check the functionality of constructor Distribution from distribution module,
-        using unitary test with the Python testing tool pytest.
+        Checks the functionality of the Distribution constructor from
+        distribution module.
     """
-
     def setup_method(self, method):
-        print('==>')
-        print(method.__doc__)
+        """Allows to see a brief description of the test in the report."""
+        print('\u21B4' + '\n' + '\u273C' + method.__doc__.strip())
 
     @pytest.fixture
     def fixture_distributions(self, scope = 'method'):
@@ -23,103 +19,101 @@ class TestDistribution():
         pytest.wrong_name = 'Normall'
         pytest.wrong_type = 'NumPyy'
 
-
-    @pytest.mark.skip(reason="Pydantic decorator does not allow create None distribution")
+    @pytest.mark.skip(
+        reason="Pydantic decorator does not allows to create a None type"
+        "distribution")
     def test_None_distribution(self):
-        """Create a None type distributions and assing the atributte constant"""
-
-        None_dist = Distribution(dist_type = None)
+        """
+        Creates a None type distribution and assigns the attribute constant
+        """
+        None_dist = Distribution(dist_type=None)
 
         assert None_dist.constant == None
 
-
     def test_constant_distribution(self):
-        """Create a constant distribution and assing the atributte constant"""
-
+        """
+        Creates a constant distribution and assigns the attribute constant.
+        """
         constant_dist = Distribution('constant')
         constant = constant_dist.constant
 
         assert constant == 0.0
 
-
     def test_empirical_distribution(self):
-        """Raising SystemError creating an empirical type distributions with a wrong filename"""
-
+        """
+        Raises a SystemError creating an empirical type distribution with a
+        wrong filename.
+        """
         FileName = 'filename'
 
         with pytest.raises(SystemError):
-            Distribution(dist_type = 'empirical', filename = FileName)
-
+            Distribution(dist_type='empirical', filename=FileName)
 
     def test_weights_distribution(self):
-        """Raising SystemError creating an empirical type distributions with a wrong filename"""
-
+        """
+        Raises a SystemError creating a weights type distribution with a
+        wrong filename.
+        """
         FileName = 'filename'
 
         with pytest.raises(SystemError):
-            assert Distribution(dist_type = 'empirical', filename = FileName)
+            assert Distribution(dist_type='weights', filename=FileName)
 
     def test_numpy_distribution(self, fixture_distributions):
-        """Create a numpy distributions and assing the atributte numpy_distribution"""
-
-        numpy_dist = \
-        Distribution(dist_type = 'numpy', dist_name = pytest.dist_name)
+        """
+        Creates a numpy distribution and assigns the attribute
+        numpy_distribution.
+        """
+        numpy_dist = Distribution(
+            dist_type='numpy', dist_name=pytest.dist_name)
         name = numpy_dist.numpy_distribution.__name__
 
         assert name == pytest.dist_name
 
-
     def test_worong_numpy_name(self, fixture_distributions):
-        """SystemError creating a numpy distribution with a wrong dist_name"""
-
+        """
+        Raises a SystemError creating a numpy distribution with a wrong
+        dist_name.
+        """
         with pytest.raises(SystemError):
-            assert Distribution(dist_type = 'numpy', dist_name = pytest.wrong_name)
-
+            assert Distribution(dist_type='numpy', dist_name=pytest.wrong_name)
 
     def test_wrong_type(self, fixture_distributions):
-        """Raising ValueError Wrong type distribution."""
-
+        """Raises a ValueError when wrong type distribution is passed."""
         with pytest.raises(ValueError):
-            assert Distribution(dist_type = pytest.wrong_type)
+            assert Distribution(dist_type=pytest.wrong_type)
 
     def test_sample_constant(self):
-        """Return a constant sample distribution."""
-
-        constant_dist = \
-        Distribution(dist_type = 'constant')
+        """Returns a constant sample distribution."""
+        constant_dist = Distribution(dist_type='constant')
         sample = constant_dist.sample()
-        expected = constant_dist.constant*np.ones(1)
+        expected = constant_dist.constant*ones(1)
 
         assert sample == expected
-
 
     def test_sample_numpy(self):
-        """Return a numpy sample distribution."""
-
-        numpy_dist = \
-        Distribution(dist_type = 'numpy', dist_name = 'beta', a = 1, b = 1)
+        """Returns a numpy sample distribution."""
+        numpy_dist = Distribution(
+            dist_type='numpy', dist_name='beta', a=1, b=1)
         sample = numpy_dist.sample()
-        expected = np.random.default_rng(seed=numpy_dist.seed).beta(1, 1)
+        expected = random.default_rng(seed=numpy_dist.seed).beta(1, 1)
 
         assert sample == expected
 
-
     def test_sample_potivie_constant(self):
-        """Return a constant positive sample distribution."""
-
-        constant_dist = \
-        Distribution(dist_type = 'constant')
+        """Returns a constant positive sample distribution."""
+        constant_dist = Distribution(dist_type='constant')
         sample = constant_dist.sample_positive()
-        expected = constant_dist.constant*np.ones(1)
+        expected = constant_dist.constant*ones(1)
 
         assert sample == expected
 
     def test_sample_positivie_numpy(self):
-        """Return a numpy positive sample distribution."""
-
-        numpy_dist = \
-        Distribution(dist_type = 'numpy', dist_name = 'normal', loc = -10, scale = 0.2)
+        """Returns a numpy positive sample distribution."""
+        numpy_dist = Distribution(
+            dist_type='numpy', dist_name='normal', loc=-10, scale=0.2)
         sample = numpy_dist.sample_positive()
-        expected = np.abs(np.random.default_rng(seed=numpy_dist.seed).normal(-10, 0.2))
+        expected = abs(
+            random.default_rng(seed=numpy_dist.seed).normal(-10, 0.2))
 
         assert sample == expected
