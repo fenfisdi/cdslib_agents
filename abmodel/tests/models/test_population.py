8bae001e73cefd533a2c5dd2976067f0e02b9fc0
VLADIMIR EMIL RUEDA GÓMEZ <vladimir.rueda@udea.edu.co>
2021-10-24 14:01:25 -0500

    test: Add the test file for population module
    This file contains unitary tests for BoxSize nametupple
    from models.

--- abmodel/tests/models/test_population.py
+++ abmodel/tests/models/test_population.py
@@ -1,14 +1,25 @@
-from unittest import TestCase
+import pytest
 
 from abmodel.models.population import BoxSize
 
 
-class BoxSizeTestCase(TestCase):
+class TestBoxSizeCase:
+    """
+        Verifies the functionality of the BoxSize namedtuple from population.
+    """
+    def setup_method(self, method):
+        """Allows to see a brief description of the test in the report."""
+        print('\u21B4' + '\n' + '\u273C' + method.__doc__.strip())
 
-    def test_box_size_instance(self):
-        result = BoxSize(0, 100, 0, 100)
+    @pytest.fixture
+    def fixture_box_size_instance(self, scope = 'method')-> None:
+        pytest.test_BoxSize = BoxSize(0, 100, 0, 100)
+        pytest.expected = [0, 100, 0, 100]
+        pytest.sides = BoxSize._fields
 
-        self.assertEqual(result.left, 0)
-        self.assertEqual(result.right, 100)
-        self.assertEqual(result.bottom, 0)
-        self.assertEqual(result.top, 100)
+    def test_box_size_instance(self, fixture_box_size_instance):
+        """Checks all values of the BoxSize are correctly assigned."""
+        for side, expected in zip(pytest.sides, pytest.expected):
+            test_BoxSize_value = getattr(pytest.test_BoxSize, side)
+
+            assert test_BoxSize_value == expected
