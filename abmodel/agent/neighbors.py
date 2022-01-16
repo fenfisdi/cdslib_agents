2ad39798ea40b56ceb8f0b5db4a04844fc388fc7
EmilRueda <vladimir.rueda@udea.edu.co>
2022-01-16 15:04:24 -0500

    docs: Adding the authors names.

--- abmodel/agent/neighbors.py
+++ abmodel/agent/neighbors.py
@@ -12,6 +12,20 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
+#
+#This package is authored by:
+#Camilo Hincapié (https://www.linkedin.com/in/camilo-hincapie-gutierrez/) (main author)
+#Ian Mejía (https://github.com/IanMejia)
+#Emil Rueda (https://www.linkedin.com/in/emil-rueda-424012207/)
+#Nicole Rivera (https://github.com/nicolerivera1)
+#Carolina Rojas Duque (https://github.com/carolinarojasd)
+#and the conceptual contributions about epidemiology of
+#Lina Marcela Ruiz Galvis (mailto:lina.ruiz2@udea.edu.co).
+#
+#Other remarkably contributors to this work were
+#Alejandro Campillo (https://www.linkedin.com/in/alucardcampillo/)
+#Daniel Alfonso Montoya (https://www.linkedin.com/in/daniel-montoya-ds/).
+
 
 from numpy import array, setdiff1d, concatenate, transpose
 # where, full, ndarray, isin, concatenate
